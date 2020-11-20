3ccb4e47c48c3fe6f28be34b478db2009ff6df75
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-11-20 09:57:05 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 radio.set_group(125)
+basic.show_string("Z SENDER")
+input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 
+def on_forever():
+    radio.send_number(input.acceleration(Dimension.Z))
+basic.forever(on_forever)
+
+
+
+
+
+
+
+
+
+"""
 def on_button_pressed_a2():
     radio.send_string("YES")
     basic.pause(200)
@@ -15,3 +30,4 @@ input.on_button_pressed(Button.B, on_button_pressed_b2)
 def on_received_string(receivedString):
     basic.show_string(receivedString)
 radio.on_received_string(on_received_string)
+"""
--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
 radio.setGroup(125)
-input.onButtonPressed(Button.A, function on_button_pressed_a2() {
-    radio.sendString("YES")
+basic.showString("Z SENDER")
+input.setAccelerometerRange(AcceleratorRange.EightG)
+basic.forever(function on_forever() {
+    radio.sendNumber(input.acceleration(Dimension.Z))
+})
+/** 
+def on_button_pressed_a2():
+    radio.send_string("YES")
     basic.pause(200)
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b2() {
-    radio.sendString("NO")
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a2)
+
+def on_button_pressed_b2():
+    radio.send_string("NO")
     basic.pause(200)
-    basic.clearScreen()
-})
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    basic.showString(receivedString)
-})
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b2)
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
+
+ */
