3b2778ca111fb3cbc26d0c8f65111913362489b4
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-11-20 09:27:05 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def on_button_pressed_a2():
-    basic.show_arrow(ArrowNames.NORTH)
+    basic.show_arrow(ArrowNames.NORTH, 500)
+    basic.pause(200)
+    basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a2)
 
 def on_button_pressed_b2():
     input.acceleration(Dimension.X)
+    basic.show_number(Dimension.X)
+    basic.pause(200)
+    basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b2)
--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 input.onButtonPressed(Button.A, function on_button_pressed_a2() {
-    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.North, 500)
+    basic.pause(200)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b2() {
     input.acceleration(Dimension.X)
+    basic.showNumber(Dimension.X)
+    basic.pause(200)
+    basic.clearScreen()
 })
