674d1055d4874c4740671c5a7f2e2ce37eea1739
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-11-20 09:46:19 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+radio.set_group(125)
+
 def on_button_pressed_a2():
-    basic.show_arrow(ArrowNames.NORTH, 500)
+    radio.send_string("YES")
     basic.pause(200)
     basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a2)
 
 def on_button_pressed_b2():
-    input.acceleration(Dimension.X)
-    basic.show_number(Dimension.X)
+    radio.send_string("NO")
     basic.pause(200)
     basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b2)
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+radio.setGroup(125)
 input.onButtonPressed(Button.A, function on_button_pressed_a2() {
-    basic.showArrow(ArrowNames.North, 500)
+    radio.sendString("YES")
     basic.pause(200)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b2() {
-    input.acceleration(Dimension.X)
-    basic.showNumber(Dimension.X)
+    radio.sendString("NO")
     basic.pause(200)
     basic.clearScreen()
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.showString(receivedString)
 })
